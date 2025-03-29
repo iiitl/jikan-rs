4cc34dca24b100c6d594cc66778c74b3666a1b95
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-03-30 00:21:22 +0530

    Added Filter Query in Genre Wrapper and Updated its tests

--- src/genre.rs
+++ src/genre.rs
@@ -1,7 +1,11 @@
-use crate::{
-    common::{DateRange, Images, Pagination},
-    JikanClient, JikanError,
-};
+use crate::{JikanClient, JikanError};
+
+pub enum GenreFilter {
+    Genres,
+    ExplicitGenres,
+    Themes,
+    Demographics,
+}
 
 use serde::{Deserialize, Serialize};
 
@@ -19,11 +23,53 @@ pub struct GenreResponse {
 }
 
 impl JikanClient {
-    pub async fn get_anime_genres(&self) -> Result<GenreResponse, JikanError> {
-        self.get("/genres/anime").await
+    pub async fn get_anime_genres(
+        &self,
+        filter: Option<GenreFilter>,
+    ) -> Result<GenreResponse, JikanError> {
+        let mut params = Vec::new();
+
+        if let Some(f) = filter {
+            let filter = match f {
+                GenreFilter::Genres => "genres",
+                GenreFilter::ExplicitGenres => "explicit_genres",
+                GenreFilter::Themes => "themes",
+                GenreFilter::Demographics => "demographics",
+            };
+            params.push(format!("filter={}", filter));
+        }
+
+        let query = if !params.is_empty() {
+            format!("?{}", params.join("&"))
+        } else {
+            String::new()
+        };
+
+        self.get(&format!("/genres/anime{}", query)).await
     }
 
-    pub async fn get_manga_genres(&self) -> Result<GenreResponse, JikanError> {
-        self.get("/genres/manga").await
+    pub async fn get_manga_genres(
+        &self,
+        filter: Option<GenreFilter>,
+    ) -> Result<GenreResponse, JikanError> {
+        let mut params = Vec::new();
+
+        if let Some(f) = filter {
+            let filter = match f {
+                GenreFilter::Genres => "genres",
+                GenreFilter::ExplicitGenres => "explicit_genres",
+                GenreFilter::Themes => "themes",
+                GenreFilter::Demographics => "demographics",
+            };
+            params.push(format!("filter={}", filter));
+        }
+
+        let query = if !params.is_empty() {
+            format!("?{}", params.join("&"))
+        } else {
+            String::new()
+        };
+
+        self.get(&format!("/genres/manga{}", query)).await
     }
 }
